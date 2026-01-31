190cb7923387de08fa58fba90ee9847dcc4f44bb
Brendan.W <brendanwinter@me.com>
2026-01-31 19:39:03 +1100

    Add files via upload

---
+++ ios/TeacherPlanner/TeacherPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeacherPlannerApp.swift
+//  TeacherPlanner
+//
+//  Created by Brendan Winter on 31/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct TeacherPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
